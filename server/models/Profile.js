79edefa41e06b272304109ed25ea10bf74d0d212
ajkatom <contact@alonadelson.com>
2022-02-03 21:14:51 -0500

    initial profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String, //to store image url
+    required: true,
+  },
+  availability: {
+    type: [{ Date }],
+    required: true,
+  },
+  rating: {
+    type: { Number, default: 0 },
+    max: 5,
+    required: true,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', profileSchema);
