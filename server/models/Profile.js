ef4dd803c2997a6c0cb8c07dee2ce74a8cb0bfb7
ajkatom <contact@alonadelson.com>
2022-02-05 19:05:20 -0500

    capatlized profile model fields and created profile controller

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema({
-  firstname: {
+  firstName: {
     type: String,
     required: true,
   },
-  lastname: {
+  lastName: {
     type: String,
     required: true,
   },
